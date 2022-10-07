def5d98972c8f5bc101f6918831b9691b162c550
chvmvd <chvmvd@users.noreply.github.com>
2022-10-07 19:33:07 +0900

    OpenInColabコンポーネントを作成

---
+++ src/components/OpenInColab/OpenInColab.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+export default function OpenInColab({path}) {
+    return (
+        <>
+        <div className={styles.root}>
+            <a href={`https://colab.research.google.com/github/chvmvd/sikepuri-of-algorithm/blob/main${path}`} target="_blank" rel="noopener">
+                <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab" />
+            </a>
+        </div>
+        </>
+    );
+}
