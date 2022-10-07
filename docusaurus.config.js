0287437bf8cf397045ec6fc549bd546670ad07df
chvmvd <chvmvd@users.noreply.github.com>
2022-10-07 21:16:08 +0900

    Prettierの実行

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,43 +1,42 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'アルゴリズム入門',
+  title: "アルゴリズム入門",
   // tagline: '',
-  url: 'https://sikepuri-of-algorithm.onrender.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  url: "https://sikepuri-of-algorithm.onrender.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
   // favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'chvmvd', // Usually your GitHub org/user name.
-  projectName: 'sikepuri-of-algorithm', // Usually your repo name.
+  organizationName: "chvmvd", // Usually your GitHub org/user name.
+  projectName: "sikepuri-of-algorithm", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'ja',
-    locales: ['ja'],
+    defaultLocale: "ja",
+    locales: ["ja"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/chvmvd/sikepuri-of-algorithm/tree/main',
+          editUrl: "https://github.com/chvmvd/sikepuri-of-algorithm/tree/main",
         },
         // blog: {
         //   showReadingTime: true,
@@ -47,7 +46,7 @@ const config = {
         //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         // },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -57,40 +56,40 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'アルゴリズム入門',
+        title: "アルゴリズム入門",
         // logo: {
         //   alt: 'My Site Logo',
         //   src: 'img/logo.svg',
         // },
         items: [
           {
-            type: 'doc',
-            docId: 'index',
-            position: 'left',
-            label: 'ドキュメント',
+            type: "doc",
+            docId: "index",
+            position: "left",
+            label: "ドキュメント",
           },
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/chvmvd/sikepuri-of-algorithm',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/chvmvd/sikepuri-of-algorithm",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Docs',
-                to: '/docs',
+                label: "Docs",
+                to: "/docs",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               // {
               //   label: 'Stack Overflow',
@@ -105,8 +104,8 @@ const config = {
               //   href: 'https://twitter.com/docusaurus',
               // },
               {
-                label: 'GitHub',
-                href: 'https://github.com/chvmvd/sikepuri-of-algorithm',
+                label: "GitHub",
+                href: "https://github.com/chvmvd/sikepuri-of-algorithm",
               },
             ],
           },
--- src/components/OpenInColab/OpenInColab.tsx
+++ src/components/OpenInColab/OpenInColab.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import styles from "./styles.module.css";
 
-export default function OpenInColab({path}) {
-    return (
-        <>
-        <div className={styles.root}>
-            <a href={`https://colab.research.google.com/github/chvmvd/sikepuri-of-algorithm/blob/main${path}`} target="_blank" rel="noopener">
-                <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab" />
-            </a>
-        </div>
-        </>
-    );
+export default function OpenInColab({ path }) {
+  return (
+    <>
+      <div className={styles.root}>
+        <a
+          href={`https://colab.research.google.com/github/chvmvd/sikepuri-of-algorithm/blob/main${path}`}
+          target="_blank"
+          rel="noopener"
+        >
+          <img
+            src="https://colab.research.google.com/assets/colab-badge.svg"
+            alt="Open In Colab"
+          />
+        </a>
+      </div>
+    </>
+  );
 }
