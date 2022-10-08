e782d36dcb133172252279cf36763a2919dd086c
chvmvd <chvmvd@users.noreply.github.com>
2022-10-08 11:24:19 +0900

    デプロイの失敗を直した

--- src/components/OpenInColab/OpenInColab.tsx
+++ src/components/OpenInColab/OpenInColab.tsx
@@ -2,12 +2,11 @@ import React from "react";
 import styles from "./styles.module.css";
 
 export default function OpenInColab({ path }) {
-  const pathname = location.pathname + path;
   return (
     <>
       <div className={styles.root}>
         <a
-          href={`https://colab.research.google.com/github/chvmvd/sikepuri-of-algorithm/blob/main${pathname}`}
+          href={`https://colab.research.google.com/github/chvmvd/sikepuri-of-algorithm/blob/main${path}`}
           target="_blank"
           rel="noopener"
         >
