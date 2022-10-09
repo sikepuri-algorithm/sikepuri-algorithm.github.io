d7d60a5085458a5b15996a98cdf1223bd3b54355
chvmvd <chvmvd@users.noreply.github.com>
2022-10-09 09:35:30 +0900

    相対パスで指定できるようにした resolved #1

--- src/components/OpenInColab/OpenInColab.tsx
+++ src/components/OpenInColab/OpenInColab.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { useLocation } from "@docusaurus/router";
 import styles from "./styles.module.css";
 
 export default function OpenInColab({ path }) {
+  const pathname = useLocation().pathname.slice(22);
   return (
     <>
       <div className={styles.root}>
         <a
-          href={`https://colab.research.google.com/github/chvmvd/sikepuri-of-algorithm/blob/main${path}`}
+          href={`https://colab.research.google.com/github/chvmvd/sikepuri-of-algorithm/blob/main${pathname}${path}`}
           target="_blank"
           rel="noopener"
         >
