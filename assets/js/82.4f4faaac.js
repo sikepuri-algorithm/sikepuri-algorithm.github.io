5ec8283d1049b83990ea9d49a2f802ccedfa2187
chvmvd <104971044+chvmvd@users.noreply.github.com>
2022-10-23 02:10:43 +0000

    Deploying to gh-pages from @ sikepuri-algorithm/sikepuri-algorithm.github.io@87dcc678d84c3074cd5b5e15e5740628f98b64c5 🚀

---
+++ assets/js/82.4f4faaac.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksikepuri_of_algorithm=self.webpackChunksikepuri_of_algorithm||[]).push([[82],{1082:t=>{t.exports=JSON.parse('{"nbformat":4,"nbformat_minor":2,"metadata":{},"cells":[{"metadata":{},"source":["print(1 + 1)"],"cell_type":"code","outputs":[{"output_type":"stream","name":"stdout","text":["2\\n"]}],"execution_count":null}]}')}}]);
