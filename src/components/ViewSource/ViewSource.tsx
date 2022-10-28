a4929aa833c2d9073ca1faf34d0959ae091e08d5
chvmvd <chvmvd@users.noreply.github.com>
2022-10-28 23:34:06 +0900

    react-jupyter-notebookを導入

--- src/components/ViewSource/ViewSource.tsx
+++ src/components/ViewSource/ViewSource.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect } from "react";
-import CodeBlock from "@theme/CodeBlock";
 import usePathname from "../usePathname";
+import JupyterViewer from "react-jupyter-notebook";
 import OpenInColab from "../OpenInColab/OpenInColab";
 
 /**
@@ -13,53 +13,22 @@ import OpenInColab from "../OpenInColab/OpenInColab";
 
 export default function ViewSource({ path, nooutput = false }) {
   const pathname = usePathname();
-  const [sources, setSources] = useState<string[]>([]);
-  const [outputs, setOutputs] = useState<string[]>([]);
+  const [content, setContent] = useState();
   useEffect(() => {
     async function tmp() {
       // 該当のipynbファイルをjson形式でとってくる
-      const content = await import(
+      const json = await import(
         `/docs/${pathname.slice(6)}${path.slice(0, -6)}.json`
       );
-      // ソースコードを取り出す
-      setSources(
-        content.cells
-          .filter((cell) => cell.cell_type === "code")
-          .map((cell) => cell.source.join(""))
-      );
-      // outputを取り出す
-      setOutputs(
-        content.cells
-          .filter((cell) => cell.cell_type === "code")
-          .map((cell) =>
-            cell.outputs.length !== 0 ? cell : { outputs: [{ text: [""] }] }
-          )
-          .map((cell) =>
-            cell.outputs.map((output) =>
-              (output.text === undefined
-                ? output.traceback === undefined
-                  ? output.data["text/plain"]
-                  : output.traceback
-                : output.text
-              ).join("")
-            )
-          )
-      );
+      setContent(json);
     }
     tmp();
   }, []);
   return (
     <>
-      {sources.map((source, i) => (
-        <React.Fragment key={i}>
-          {/* ソースコード */}
-          <CodeBlock language="python">{source}</CodeBlock>
-          {/* output */}
-          {!nooutput && (
-            <CodeBlock language="plaintext">{outputs[i]}</CodeBlock>
-          )}
-        </React.Fragment>
-      ))}
+      {content !== undefined && (
+        <JupyterViewer rawIpynb={content} language="python" />
+      )}
       <OpenInColab path={path} />
     </>
   );
