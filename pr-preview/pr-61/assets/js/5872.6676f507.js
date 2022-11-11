0d987ca62d263f6650e98b600ea3cbb6fcdd8ad9
chvmvd <chvmvd@users.noreply.github.com>
2022-11-11 14:57:40 +0000

    Deploy preview for PR 61 🛫

---
+++ pr-preview/pr-61/assets/js/5872.6676f507.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksikepuri_of_algorithm=self.webpackChunksikepuri_of_algorithm||[]).push([[5872],{35872:e=>{e.exports=JSON.parse('{"nbformat":4,"nbformat_minor":2,"metadata":{},"cells":[{"metadata":{},"source":["scores = [96, 56, 42, 74, 86]\\n","scores[0] = 37"],"cell_type":"code","outputs":[],"execution_count":null}]}')}}]);
