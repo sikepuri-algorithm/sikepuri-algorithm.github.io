a9fa0b271a38f9b94569e7c94d6bbeb2d7f207ff
chvmvd <chvmvd@users.noreply.github.com>
2022-11-24 05:50:15 +0000

    Deploy preview for PR 68 🛫

---
+++ pr-preview/pr-68/assets/js/1402.8f813feb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksikepuri_of_algorithm=self.webpackChunksikepuri_of_algorithm||[]).push([[1402],{51402:t=>{t.exports=JSON.parse('{"nbformat":4,"nbformat_minor":2,"metadata":{},"cells":[{"metadata":{},"source":["f = False\\n","print(f)"],"cell_type":"code","outputs":[{"output_type":"stream","name":"stdout","text":["False\\n"]}],"execution_count":null}]}')}}]);
