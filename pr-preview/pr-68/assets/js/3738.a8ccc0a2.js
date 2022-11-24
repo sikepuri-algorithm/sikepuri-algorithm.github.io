460d72e6d38cb61891ca4fd2b21f255d24baaa4c
chvmvd <chvmvd@users.noreply.github.com>
2022-11-24 05:53:47 +0000

    Remove preview for PR 68 🛬

--- pr-preview/pr-68/assets/js/3738.a8ccc0a2.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksikepuri_of_algorithm=self.webpackChunksikepuri_of_algorithm||[]).push([[3738],{43738:t=>{t.exports=JSON.parse('{"nbformat":4,"nbformat_minor":2,"metadata":{},"cells":[{"metadata":{},"source":["print(sum([1, 2, 3]))"],"cell_type":"code","outputs":[{"output_type":"stream","name":"stdout","text":["6\\n"]}],"execution_count":null}]}')}}]);
