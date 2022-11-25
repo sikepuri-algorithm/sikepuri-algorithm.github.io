889a379204d2fc1c6741a693ece2faf85af8c4c2
chvmvd <chvmvd@users.noreply.github.com>
2022-11-26 01:41:00 +0900

    make lifegame little

--- src/theme/ReactLiveScope/index.js
+++ src/theme/ReactLiveScope/index.js
@@ -1,6 +1,17 @@
 import React from "react";
-import LifeGame from "@site/src/components/LifeGame/LifeGame";
+import LifeGameTmp from "@site/src/components/LifeGame/LifeGame";
 // Add react-live imports you need here
+
+function LifeGame() {
+  return (
+    <>
+      <div style={{ maxWidth: "200px" }}>
+        <LifeGameTmp />
+      </div>
+    </>
+  );
+}
+
 const ReactLiveScope = {
   React,
   ...React,
