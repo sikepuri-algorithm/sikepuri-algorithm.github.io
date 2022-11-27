2592da37fabf1f368938e81ff374f6d537e4cd1d
chvmvd <chvmvd@users.noreply.github.com>
2022-11-27 13:26:53 +0000

    Deploy preview for PR 77 🛫

---
+++ pr-preview/pr-77/assets/js/1652.09351cb5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksikepuri_of_algorithm=self.webpackChunksikepuri_of_algorithm||[]).push([[1652],{31652:t=>{t.exports=JSON.parse('{"nbformat":4,"nbformat_minor":2,"metadata":{},"cells":[{"metadata":{},"source":["print(sum([2, 5, 7]))"],"cell_type":"code","outputs":[{"output_type":"stream","name":"stdout","text":["14\\n"]}],"execution_count":null}]}')}}]);
