57eb8130f30bb03c7028f35cafec9b2559f93c3c
chvmvd <chvmvd@users.noreply.github.com>
2022-11-28 00:11:47 +0900

    create html,css,js InteractiveCodeEditor

--- src/components/InteractiveCodeEditor/InteractiveCodeEditor.tsx
+++ src/components/InteractiveCodeEditor/InteractiveCodeEditor.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import Tabs from "@theme/Tabs";
+import TabItem from "@theme/TabItem";
 import Editor from "@monaco-editor/react";
 import MarkdownIt from "markdown-it";
 import BrowserWindow from "@site/src/components/BrowserWindow";
@@ -7,13 +9,22 @@ import styles from "./styles.module.css";
 export default function InteractiveCodeEditor({
   language,
   defaultValue,
+  defaultHTML,
+  defaultCSS,
+  defaultJavaScript,
 }: {
-  language: "html" | "markdown";
-  defaultValue: string;
+  language: "html" | "markdown" | "HTML-CSS-JavaScript";
+  defaultValue?: string;
+  defaultHTML?: string;
+  defaultCSS?: string;
+  defaultJavaScript?: string;
 }) {
   const [code, setCode] = useState<string>(defaultValue);
+  const [html, setHTML] = useState<string>(defaultHTML);
+  const [css, setCSS] = useState<string>(defaultCSS);
+  const [js, setJS] = useState<string>(defaultJavaScript);
   const md = new MarkdownIt({ html: true, linkify: true, typographer: true });
-  const css = `
+  const markdownCSS = `
 <style>
   blockquote {
     margin: 0;
@@ -49,14 +60,49 @@ export default function InteractiveCodeEditor({
       <div className={styles.playgroundContainer}>
         <div className={styles.playgroundHeader}>ライブエディター</div>
         <div className={styles.playgroundEditor}>
-          <Editor
-            height="200px"
-            defaultLanguage={language}
-            defaultValue={defaultValue}
-            onChange={(value) => {
-              setCode(value);
-            }}
-          />
+          {language === "HTML-CSS-JavaScript" ? (
+            <Tabs groupId="editor">
+              <TabItem value="html" label="HTML">
+                <Editor
+                  height="200px"
+                  defaultLanguage="html"
+                  defaultValue={defaultHTML}
+                  onChange={(value) => {
+                    setCode(value);
+                  }}
+                />
+              </TabItem>
+              <TabItem value="css" label="CSS">
+                <Editor
+                  height="200px"
+                  defaultLanguage="css"
+                  defaultValue={defaultCSS}
+                  onChange={(value) => {
+                    setCode(value);
+                  }}
+                />
+              </TabItem>
+              <TabItem value="js" label="JavaScript">
+                <Editor
+                  height="200px"
+                  defaultLanguage="javascript"
+                  defaultValue={defaultJavaScript}
+                  onChange={(value) => {
+                    setCode(value);
+                  }}
+                />
+              </TabItem>
+            </Tabs>
+          ) : (
+            <Editor
+              height="200px"
+              defaultLanguage={language}
+              defaultValue={defaultValue}
+              onChange={(value) => {
+                setCode(value);
+              }}
+            />
+          )}
         </div>
         <div className={styles.playgroundHeader}>結果</div>
         <div className={styles.playgroundPreview}>
@@ -64,7 +110,13 @@ export default function InteractiveCodeEditor({
             <iframe
               width="100%"
               height="100%"
-              srcDoc={language === "html" ? code : css + md.render(code)}
+              srcDoc={
+                language === "html"
+                  ? code
+                  : language === "markdown"
+                  ? markdownCSS + md.render(code)
+                  : `<style>${css}</style>${html}<script>${js}</script>`
+              }
               title="Live Code"
             ></iframe>
           </BrowserWindow>
