0239101f449714022d2b8803f0b7f35f6b627f40
chvmvd <chvmvd@users.noreply.github.com>
2022-11-27 15:34:51 +0000

    Deploy preview for PR 79 🛫

---
+++ pr-preview/pr-79/assets/js/8025.f7e38214.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksikepuri_of_algorithm=self.webpackChunksikepuri_of_algorithm||[]).push([[8025],{28025:t=>{t.exports=JSON.parse('{"nbformat":4,"nbformat_minor":2,"metadata":{},"cells":[{"metadata":{},"source":["print(1 + 1)"],"cell_type":"code","outputs":[{"output_type":"stream","name":"stdout","text":["2\\n"]}],"execution_count":null}]}')}}]);
