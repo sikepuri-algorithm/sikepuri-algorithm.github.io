d978c546a25bbde420c363e5bb7434c1e17b9544
chvmvd <chvmvd@users.noreply.github.com>
2022-11-28 02:16:03 +0000

    Remove preview for PR 81 🛬

--- pr-preview/pr-81/assets/js/1448.4065f394.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksikepuri_of_algorithm=self.webpackChunksikepuri_of_algorithm||[]).push([[1448],{11448:t=>{t.exports=JSON.parse('{"nbformat":4,"nbformat_minor":2,"metadata":{},"cells":[{"metadata":{},"source":["f = False\\n","print(f)"],"cell_type":"code","outputs":[{"output_type":"stream","name":"stdout","text":["False\\n"]}],"execution_count":null}]}')}}]);
