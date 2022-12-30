526b339c7802556b2fa21bd740a611fe0b6f7422
chvmvd <chvmvd@users.noreply.github.com>
2022-12-30 12:20:29 +0900

    Add html-javascript function in InteractiveCodeEditor

--- src/components/InteractiveCodeEditor/index.tsx
+++ src/components/InteractiveCodeEditor/index.tsx
@@ -99,13 +99,58 @@ function HtmlCssEditor({
   );
 }
 
+function HtmlJavascriptEditor({
+  defaultHTML,
+  setHTML,
+  defaultJavaScript,
+  setJS,
+}: {
+  defaultHTML: string;
+  setHTML: Dispatch<SetStateAction<string>>;
+  defaultJavaScript: string;
+  setJS: Dispatch<SetStateAction<string>>;
+}) {
+  return (
+    <>
+      <Tabs groupId="editor">
+        <TabItem value="html" label="HTML">
+          <Editor
+            height="400px"
+            defaultLanguage="html"
+            defaultValue={defaultHTML}
+            onChange={(value) => {
+              setHTML(value);
+            }}
+          />
+        </TabItem>
+        <TabItem value="js" label="JavaScript">
+          <Editor
+            height="400px"
+            defaultLanguage="javascript"
+            defaultValue={defaultJavaScript}
+            onChange={(value) => {
+              setJS(value);
+            }}
+          />
+        </TabItem>
+      </Tabs>
+    </>
+  );
+}
+
 export default function InteractiveCodeEditor({
   language,
   children,
   defaultCSS,
   defaultJavaScript,
 }: {
-  language: "markdown" | "html" | "html-css" | "html-css-javascript" | "latex";
+  language:
+    | "markdown"
+    | "html"
+    | "html-css"
+    | "html-javascript"
+    | "html-css-javascript"
+    | "latex";
   children?: string;
   defaultCSS?: string;
   defaultJavaScript?: string;
@@ -135,6 +180,13 @@ export default function InteractiveCodeEditor({
               defaultCSS={defaultCSS}
               setCSS={setCSS}
             />
+          ) : language === "html-javascript" ? (
+            <HtmlJavascriptEditor
+              defaultHTML={children}
+              setHTML={setCode}
+              defaultJavaScript={defaultJavaScript}
+              setJS={setJS}
+            />
           ) : (
             <Editor
               height="400px"
@@ -157,6 +209,8 @@ export default function InteractiveCodeEditor({
               ? latexToHTML(code)
               : language === "html-css"
               ? `<style>${css}</style>${code}`
+              : language === "html-javascript"
+              ? `${code}<script>${js}</script>`
               : `<style>${css}</style>${code}<script>${js}</script>`}
           </IframeOutput>
         </div>
